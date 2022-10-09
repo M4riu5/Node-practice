ca1cfabb7f532047b895ebc6419cce9080aa40b3
Marius <mariushaf@gmail.com>
2022-10-09 14:19:56 +0300

    crud done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const app = express()
 
+connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
